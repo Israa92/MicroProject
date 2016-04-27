1f9faf76056c5d925556e216e1594ad0720ced5a
TensaeYoseph <tensaebe@yahoo.com>
2016-04-27 14:16:07 +0200

    int

--- src/MikroProject/Startup.cs
+++ src/MikroProject/Startup.cs
@@ -16,6 +16,7 @@ namespace MikroProject
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
 
         }
 
