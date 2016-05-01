db12bca45d742e399f1b52c74bc6667d2b305c35
parisa Carlborg <parak80@gmail.com>
2016-05-01 19:13:12 +0200

    changes

---
+++ src/MikroProject/ViewModels/StrudentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MikroProject.ViewModels
+{
+    public class StrudentViewModel
+    {
+        public int Id { get; set; }
+    }
+}
