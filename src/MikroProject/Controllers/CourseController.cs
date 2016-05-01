129551694a055880547322c7b3105d04464e97d5
parisa Carlborg <parak80@gmail.com>
2016-05-01 21:28:43 +0200

    Course, CourseController, viewmodel, vyer

--- src/MikroProject/Controllers/CourseController.cs
+++ src/MikroProject/Controllers/CourseController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using MikroProject.Models;
+using MikroProject.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,8 +12,37 @@ namespace MikroProject.Controllers
 {
     public class CourseController : Controller
     {
+        AdminContext _context;
+        public CourseController(AdminContext context)
+        {
+            _context = context;
+        }
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult CreateInstructor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateCourse(CourseViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var course = new Course();
+            course.Name = model.Name;
+            course.Id = model.Id;
+            course.Instructor = model.Instructor;
+            course.Class = model.Class;
+            course.StartDate = model.StartDate;
+            course.EndTime = model.EndTime;
+
+            // Add to DB
+            _context.Course.Add(course);
+            _context.SaveChanges();
+
+            return RedirectToAction("CreatedCourse");
+        }
+        public IActionResult CreatedCourse()
         {
             return View();
         }
--- src/MikroProject/ViewModels/CourseViewModel.cs
+++ src/MikroProject/ViewModels/CourseViewModel.cs
@@ -8,5 +8,10 @@ namespace MikroProject.ViewModels
     public class CourseViewModel
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Instructor { get; set; }
+        public string Class { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }
