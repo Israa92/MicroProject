249a9e1c296f8519c719ad38214aad2355ff30be
parisa Carlborg <parak80@gmail.com>
2016-05-01 20:37:50 +0200

    StudentsController, CreateStudent, CreatedStudent

--- src/MikroProject/Controllers/StudentsController.cs
+++ src/MikroProject/Controllers/StudentsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using MikroProject.ViewModels;
+using MikroProject.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,8 +12,38 @@ namespace MikroProject.Controllers
 {
     public class StudentsController : Controller
     {
+        AdminContext _context;
+        public StudentsController(AdminContext context)
+        {
+            _context = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(StudentViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var student = new Student();
+            student.Id = model.Id;
+
+            // Add to DB
+            _context.Student.Add(student);
+            _context.SaveChanges();
+
+            return RedirectToAction("CreatedStudent");
+
+        }
+        public IActionResult CreatedStudent()
         {
             return View();
         }
--- src/MikroProject/ViewModels/StudentViewModel.cs
+++ src/MikroProject/ViewModels/StudentViewModel.cs
@@ -8,5 +8,8 @@ namespace MikroProject.ViewModels
     public class StudentViewModel
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int IDNumber { get; set; }
     }
 }
