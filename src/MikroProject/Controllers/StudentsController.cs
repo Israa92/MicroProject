b2cab530a3f285376c2b9f16d9c4653b86e7b332
israa92 <israa.alazam@yahoo.se>
2016-05-01 20:30:26 +0200

    edit

--- src/MikroProject/Controllers/StudentsController.cs
+++ src/MikroProject/Controllers/StudentsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using MikroProject.ViewModels;
+using MikroProject.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,43 @@ namespace MikroProject.Controllers
 {
     public class StudentsController : Controller
     {
+        AdminContext context;
         // GET: /<controller>/
-        public IActionResult Index()
+        public StudentViewModel Edit(int id)
         {
-            return View();
+            var student = context.Student.FirstOrDefault(s => s.Id == id);
+            return new StudentViewModel
+            {
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                IDNumber = student.IDNumber,
+                Class = student.Class,
+            };
+        }
+
+        public void Update(StudentViewModel editModel)
+        {
+            var student = context.Student.FirstOrDefault(s => s.Id == editModel.Id);
+            student.FirstName = editModel.FirstName;
+            student.LastName = editModel.LastName;
+            student.IDNumber = editModel.IDNumber;
+            student.Class = editModel.Class;
+
+            context.SaveChanges();
+        }
+
+        public StudentViewModel[] GetAllStudents()
+        {
+            return context.Student
+                .Select(r => new StudentViewModel
+                {
+                    Id = r.Id,
+                    FirstName = r.FirstName,
+                    LastName = r.LastName,
+                    IDNumber = r.IDNumber,
+                    Class = r.Class,
+                })
+            .ToArray();
         }
     }
 }
---
+++ src/MikroProject/ViewModels/StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MikroProject.ViewModels
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int IDNumber { get; set; }
+        public string Class { get; set; }
+    }
+}
